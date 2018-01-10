2cc2b4ec9d4ea6856c34f3eda26837c073242f41
farpras <farpras48@live.com>
2018-01-10 12:19:21 +0700

    Add quote

--- js/quotes.js
+++ js/quotes.js
@@ -5,6 +5,7 @@ Quotation[1] = "\"The greatest trick the devil ever pulled was convincing the wo
 Quotation[2] = "\"It's funny how everyone that LIES gets POPULAR and everyone that tells the TRUTH is a f*cking PSYCHO!\"<br />- Anonymous";
 Quotation[3] = "\"Be alone, that is the secret of invention. Be alone, that is when ideas are born.\"<br />- Nikola Tesla";
 Quotation[4] = "\"It's better to walk alone than with a crowd going in the wrong direction.\"<br />- Anonymous";
+Quotation[5] = "\"If Wars can be started by LIES, Peace can be started by TRUTH.\"<br />- Julian Assange";
 
 var Q = Quotation.length;
 var whichQuotation=Math.round(Math.random()*(Q-1));
