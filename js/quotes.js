aa0df5dbe1d4c1bd28d2fde375ba19fd4628cbb2
farpras <farpras48@live.com>
2018-01-19 21:45:44 +0700

    + quotes

--- js/quotes.js
+++ js/quotes.js
@@ -6,6 +6,11 @@ Quotation[2] = "\"It's funny how everyone that LIES gets POPULAR and everyone th
 Quotation[3] = "\"Be alone, that is the secret of invention. Be alone, that is when ideas are born.\"<br />- Nikola Tesla";
 Quotation[4] = "\"It's better to walk alone than with a crowd going in the wrong direction.\"<br />- Anonymous";
 Quotation[5] = "\"If Wars can be started by LIES, Peace can be started by TRUTH.\"<br />- Julian Assange";
+Quotation[6] = "\"Overthinking is the BIGGEST source of UNHAPPINESS.\"<br />- Anonymous";
+Quotation[7] = "\"Until we extend our circle of compassion to all living beings, humanity will not find peace.\"<br />- Albert Schweitzer";
+Quotation[8] = "\"The creative adult is the child who survived.\"<br />- Anonymous";
+Quotation[9] = "\"Fake LOVE is worse than real HATE.\"<br />- Anonymous";
+Quotation[10] = "\"Human is the only creature who needs money to stay alive.\"<br />- Anonymous";
 
 var Q = Quotation.length;
 var whichQuotation=Math.round(Math.random()*(Q-1));
